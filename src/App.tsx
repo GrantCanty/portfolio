4fe835cf35f0f58ed4eef74f1231339625d8ecd3
Grant Canty <cheoso@Grants-MacBook-Pro.local>
2025-01-25 22:23:41 +0100

    added sections in section 2. added navigation to each section. removed section 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import {Route, Routes} from 'react-router-dom';
 import './styles/App.css'
+//import Home from './pages/home'
 import Home from './pages/home'
 
 function App() {
--- src/assets/navigation.tsx
+++ src/assets/navigation.tsx
@@ -2,13 +2,13 @@ import { ReactElement } from 'react';
 
 const Navigation: React.FC = (): ReactElement => {
     return(
-        <>
+        <nav>
             <ul>
-                <li>About</li>
-                <li>Portfolio</li>
-                <li>Resume</li>
+                <li> <a href='#about'>About </a></li>
+                <li><a href='#portfolio' >Portfolio </a></li>
+                <li><a href='#resume'>Resume </a></li>
             </ul>
-        </>
+        </nav>
     )
 }
 
