fe7b0d332b253a65b7832f1fc6fddc2c706fad33
Grant Canty <cheoso@Grants-MacBook-Pro.local>
2025-01-26 02:13:32 +0100

    added support to be able to switch languages. stil needs a lot of work, however

--- src/assets/about.tsx
+++ src/assets/about.tsx
@@ -1,12 +1,16 @@
 import { ReactElement } from 'react';
 import text from '../data/text.json'
 
+interface Props {
+    lang: string;
+}
+
 let info = Object(text)
 
-const About: React.FC = (): ReactElement => { 
+const About: React.FC<Props> = (props): ReactElement => { 
     return(
         <div id='about' className='section about'>
-            {info['en']['about'].map((ele: string, key: number) => {
+            {info[props.lang]['about'].map((ele: string, key: number) => {
                 return <p key={key}>{ele}</p>
             })}
         </div>
