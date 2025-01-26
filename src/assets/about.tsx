d50096d0ddd7464164691f94faf994097fcc12ef
Grant Canty <cheoso@Grants-MacBook-Pro.local>
2025-01-26 01:02:12 +0100

    updates to about component. json file for text info

--- src/assets/about.tsx
+++ src/assets/about.tsx
@@ -1,18 +1,20 @@
 import { ReactElement } from 'react';
+import text from '../data/text.json'
 
 const About: React.FC = (): ReactElement => {
+    let info = Object(text)
+
+    info['en']['about'].forEach((ele: string) => {
+        console.log(ele)
+    })
+    
     return(
         <div id='about' className='section about'>
-            <h2>About</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae nostrum delectus labore, quo consequuntur illum facere itaque adipisci fugit possimus totam doloribus, expedita sequi maiores libero quaerat provident quia voluptas.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae nostrum delectus labore, quo consequuntur illum facere itaque adipisci fugit possimus totam doloribus, expedita sequi maiores libero quaerat provident quia voluptas.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae nostrum delectus labore, quo consequuntur illum facere itaque adipisci fugit possimus totam doloribus, expedita sequi maiores libero quaerat provident quia voluptas.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae nostrum delectus labore, quo consequuntur illum facere itaque adipisci fugit possimus totam doloribus, expedita sequi maiores libero quaerat provident quia voluptas.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae nostrum delectus labore, quo consequuntur illum facere itaque adipisci fugit possimus totam doloribus, expedita sequi maiores libero quaerat provident quia voluptas.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae nostrum delectus labore, quo consequuntur illum facere itaque adipisci fugit possimus totam doloribus, expedita sequi maiores libero quaerat provident quia voluptas.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae nostrum delectus labore, quo consequuntur illum facere itaque adipisci fugit possimus totam doloribus, expedita sequi maiores libero quaerat provident quia voluptas.</p>
+            {info['en']['about'].map((ele: string) => {
+                return <p>{ele}</p>
+            })}
         </div>
     )
 }
 
-export default About
+export default About;
--- src/assets/me.tsx
+++ src/assets/me.tsx
@@ -4,8 +4,8 @@ const Me: React.FC = (): ReactElement => {
     return(
         <>
             <h1>Grant Canty</h1>
-            <h2>Developer</h2>
-            <h2>Master's student</h2>
+            <h2>AI Master's Student</h2>
+            <h2>Aspiring Developer</h2>
         </>
     )
 }
