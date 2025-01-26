0a41b81f37035c1c19edab5e8c7d7fc13fba6302
Grant Canty <cheoso@Grants-MacBook-Pro.local>
2025-01-26 01:39:26 +0100

    small changes to me and display text. able to scroll from anywhere on the page now

--- src/assets/me.tsx
+++ src/assets/me.tsx
@@ -4,7 +4,7 @@ const Me: React.FC = (): ReactElement => {
     return(
         <>
             <h1>Grant Canty</h1>
-            <h2>AI Master's Student</h2>
+            <h2>Master's Student in AI</h2>
             <h2>Aspiring Developer</h2>
         </>
     )
